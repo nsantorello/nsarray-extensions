583d89f468b7311343dc5a191e1496a17c70c0f2
Noah Santorello <nsantorello@gmail.com>
2011-08-12 00:44:41 -0400

    Added contains method and cleaned up all method.

--- NSArrayExtensions/NSArrayExtensions/NSArray+Extensions.m
+++ NSArrayExtensions/NSArrayExtensions/NSArray+Extensions.m
@@ -6,22 +6,37 @@
 //  Copyright 2011 Noah Santorello. All rights reserved.
 //
 
+// TODO: (ns): throw exceptions within functions if fn == nil
+
 #import "NSArray+Extensions.h"
 
 @implementation NSArray (Extensions)
 
 -(BOOL)all:(BOOL(^)(id obj))fn
+{    
+    __block BOOL all = true;
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        all = all && fn(obj);
+    }];
+    
+    return all;
+}
+
+-(BOOL)contains:(BOOL(^)(id obj))fn
 {
-    NSNumber* trueNum = [NSNumber numberWithBool:TRUE];
-    return [[self foldl:trueNum fn:^id(id acc, id obj) {
-        //return [NSNumber numberWithBool:([((NSNumber*)acc) boolValue] && [[NSNumber numberWithBool:fn(obj)] boolValue])];//[((NSNumber*)obj) boolValue])];
-        return [NSNumber numberWithBool:([((NSNumber*)acc) boolValue] && fn(obj))];
-    }] boolValue];
+    __block BOOL contains = NO;
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        *stop = contains = contains || fn(obj);
+    }];
+    
+    return contains;
 }
 
 -(id)foldl:(id) acc fn:(id(^)(id acc, id obj))fn
 {
-    id __block blockAcc = acc;
+    __block id blockAcc = acc;
     [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         blockAcc = [fn(blockAcc, obj) retain];
     }];
