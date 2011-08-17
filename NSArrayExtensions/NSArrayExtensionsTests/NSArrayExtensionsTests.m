47e63bda67c09a5c9a06445e0e2814ecd5c2d88f
Noah Santorello <nsantorello@gmail.com>
2011-08-17 12:33:16 -0400

    Reorganized unique:fn unit tests

--- NSArrayExtensions/NSArrayExtensionsTests/NSArrayExtensionsTests.m
+++ NSArrayExtensions/NSArrayExtensionsTests/NSArrayExtensionsTests.m
@@ -12,6 +12,8 @@
 
 @synthesize empty, oneElem, numbers, people;
 
+#pragma mark - Setup/Teardown
+
 - (void)setUp
 {
     [super setUp];
@@ -80,41 +82,50 @@
     [super tearDown];
 }
 
--(void)testUnique
+#pragma mark - unique:fn
+
+-(void)testUniqueEmpty
 {
-    BOOL eq;
-    
     id unique_empty = [empty unique:^id(id obj) {
         return obj;
     }];
-    eq = [empty elementsEqual:unique_empty];
+    BOOL eq = [empty elementsEqual:unique_empty];
     STAssertTrue(eq, @"Unique of empty array should be empty array");
-    
+}
+
+-(void)testUniqueOneElem
+{
     id unique_oneElem = [oneElem unique:^id(id obj) {
         return obj;
     }];
-    eq = [oneElem elementsEqual:unique_oneElem];
+    BOOL eq = [oneElem elementsEqual:unique_oneElem];
     STAssertTrue(eq, @"Unique of single element array should be the same");
-    
+}
+
+-(void)testUniqueNumbers
+{
     id unique_numbers = [numbers unique:^id(id obj) {
         return obj;
     }];
-    eq = [unique_numbers count] == 9 && [[numbers reduce:[NSNumber numberWithBool:YES] fn:^id(id acc, id obj) {
+    BOOL eq = [unique_numbers count] == 9 && [[numbers reduce:[NSNumber numberWithBool:YES] fn:^id(id acc, id obj) {
         return [NSNumber numberWithBool:([acc boolValue] && [unique_numbers contains:^BOOL(id obj2) {
             return [obj isEqual:obj2];
         }])];
     }] boolValue];
-    STAssertTrue(eq, @"Unique test 1 with numbers: %@", unique_numbers);
-    
+    STAssertTrue(eq, @"Unique test with numbers: %@", unique_numbers);
+}
+
+-(void)testUniquePeople
+{
     id unique_people = [people unique:^id(id obj) {
         return ((Person*)obj).name;
     }];
-    eq = [unique_people count] == 6 && [[people reduce:[NSNumber numberWithBool:YES] fn:^id(id acc, id obj) {
+    BOOL eq = [unique_people count] == 6 && [[people reduce:[NSNumber numberWithBool:YES] fn:^id(id acc, id obj) {
         return [NSNumber numberWithBool:([acc boolValue] && [unique_people contains:^BOOL(id obj2) {
             return [((Person*)obj).name isEqualToString:((Person*)obj2).name];
         }])];
     }] boolValue];
-    STAssertTrue(eq, @"Unique test 2 with people's name: %@", unique_people);
+    STAssertTrue(eq, @"Unique test with people's name: %@", unique_people);
     
     id unique_people_age = [people unique:^id(id obj) {
         return ((Person*)obj).age;
@@ -124,7 +135,7 @@
             return [((Person*)obj).age isEqual:((Person*)obj2).age];
         }])];
     }] boolValue];
-    STAssertTrue(eq, @"Unique test 3 with people's age: %@", unique_people_age);
+    STAssertTrue(eq, @"Unique test with people's age: %@", unique_people_age);
     
     id unique_people_bestFriend = [people unique:^id(id obj) {
         return ((Person*)obj).bestFriend.name;
@@ -134,7 +145,8 @@
             return [((Person*)obj).bestFriend.name isEqualToString:((Person*)obj2).bestFriend.name];
         }])];
     }] boolValue];
-    STAssertTrue(eq, @"Unique test 4 with people's best friend: %@", unique_people_bestFriend);
+    STAssertTrue(eq, @"Unique test with people's best friend: %@", unique_people_bestFriend);
 }
+
 
 @end
